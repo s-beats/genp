f118d195e037ed072b3650bc0d9904f05b73dd59
s-beats <sakashin10291029@gmail.com>
2022-08-01 22:07:59 +0900

    feat: execute template

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "io/ioutil"
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"text/template"
+)
 
 func main() {
-	ioutil.WriteFile("test.go", []byte("This is test"), 0644)
+	tpl, err := template.ParseFiles("test.tmpl")
+	if err != nil {
+		log.Fatal("Failed to generate template: ", err)
+	}
+
+	m := map[string]interface{}{
+		"Name": "test",
+		"Age":  30,
+	}
+
+	buf := bytes.NewBuffer(nil)
+
+	if err := tpl.Execute(buf, m); err != nil {
+		log.Fatal("Failed to execute template: ", err)
+	}
+	ioutil.WriteFile("test.go", buf.Bytes(), 0644)
 }
